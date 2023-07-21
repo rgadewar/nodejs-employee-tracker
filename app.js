3a7fed114ef66970904fd5ba2dc69bfed45a8f2e
Rupa Gadewar <rupa@gadewar.com>
2023-07-21 16:54:48 -0500

    Minor change

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
+// Define the root route
+app.get('/', (req, res) => {
+  res.send('Hello, this is the root page!');
+});
+
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
